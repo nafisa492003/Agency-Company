079507e0ae7e6bc0be94e6a76be7f89e79f24614
nafisa492003 <afifayasmin2003@gmail.com>
2024-06-01 20:48:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav_bar from './Components/Nav_bar'
+
+const App = () => {
+  return (
+    <>
+      <Nav_bar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Nav_bar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav_bar = () => {
+  return (
+    <div>Nav_bar</div>
+  )
+}
+
+export default Nav_bar
